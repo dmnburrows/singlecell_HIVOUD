5e0843941bbe09ea9f10abf4a938de0bf7f42fa6
dmnburrows <dominicburrows@hotmail.co.uk>
2023-09-18 18:14:22 -0700

    running new conversion seurat to h5

--- workspace.R
+++ workspace.R
@@ -1,11 +1,12 @@
 library(Seurat)
+library(SeuratData)
+library(SeuratDisk)
+
 #load rds file into seurat
 data <- readRDS("/cndd3/dburrows/DATA/singlecell_HIVOUD/230828_PFC_13samples_HVWs_combined.rds")
-
-library(SeuratDisk)
-#convert seurat object to loom
-SeuratDisk::as.loom(data, filename = "/cndd3/dburrows/DATA/singlecell_HIVOUD/230828_PFC_13samples_HVWs_combined.loom")
-
+SaveH5Seurat(data, filename = "/cndd3/dburrows/DATA/singlecell_HIVOUD/230828_PFC_13samples_HVWs_combined.h5Seurat")
+Convert("/cndd3/dburrows/DATA/singlecell_HIVOUD/230828_PFC_13samples_HVWs_combined.h5Seurat", assay="RNA", dest = "/cndd3/dburrows/DATA/singlecell_HIVOUD/RNA_230828_PFC_13samples_HVWs_combined.h5ad")
+Convert("/cndd3/dburrows/DATA/singlecell_HIVOUD/230828_PFC_13samples_HVWs_combined.h5Seurat", assay="ATAC", dest = "/cndd3/dburrows/DATA/singlecell_HIVOUD/ATAC_230828_PFC_13samples_HVWs_combined.h5ad")
 
 
 
